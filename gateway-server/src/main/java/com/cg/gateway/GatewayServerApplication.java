70dd691be933808a09eb0bab3adddc70ea7c9558
rex.tan <rex.tan@infox.com.cn>
2019-07-04 16:24:25 +0800

    fix

--- gateway-server/src/main/java/com/cg/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/cg/gateway/GatewayServerApplication.java
@@ -2,7 +2,10 @@ package com.cg.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 网关服务
@@ -16,4 +19,10 @@ public class GatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
+	
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
