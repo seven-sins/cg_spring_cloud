5d67ed894f887688d4974a65081386d238ebf997
rex.tan <rex.tan@infox.com.cn>
2019-07-30 14:26:16 +0800

    fix

--- flink/src/main/java/com/cg/flink/FlinkApplication.java
+++ flink/src/main/java/com/cg/flink/FlinkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.cg.common.annotation.EnableExceptionHandler;
 import com.cg.common.annotation.EnableMvnConfig;
 import com.cg.common.annotation.EnableSpringUtil;
+import com.cg.flink.stream.task.UserVisitTask;
 
 /**
  * springUtil工具
@@ -24,18 +25,11 @@ import com.cg.common.annotation.EnableSpringUtil;
 public class FlinkApplication implements CommandLineRunner {
 	
 	public static void main(String[] args) {
-		// System.setProperty("es.set.netty.runtime.available.processors", "false"); // 不加这行es启动报错
 		SpringApplication.run(FlinkApplication.class, args);
 	}
 
-	// 部署  --classpath file:///home/hadoop/lib/accessors-smart-1.2.jar  --classpath file:///home/hadoop/lib/akka-actor_2.11-2.4.20.jar 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
-
+		UserVisitTask.main(args);
 	}
-	
-
-
 }
